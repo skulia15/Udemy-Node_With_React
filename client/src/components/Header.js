f118dea616472d26a7fd44c1aac5801cf3474f55
Skúli Arnarsson <skulia15@ru.is>
2020-01-02 08:41:54 +0000

    Node backend, store and authentication up and ready

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return '';
+            case false:
+                return (
+                    <li>
+                        <a href="/auth/google">Login With Google</a>
+                    </li>
+                );
+            default:
+                return (
+                    <li>
+                        <a href="/api/logout">Logout</a>
+                    </li>
+                );
+        }
+    }
     render() {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    <div className="left brand-logo">Emaily</div>
-                    <ul className="right">
-                        <li>
-                            <a>Login With Google</a>
-                        </li>
-                    </ul>
+                    <a className="left brand-logo">Emaily</a>
+                    <ul className="right">{this.renderContent()}</ul>
                 </div>
             </nav>
         );
     }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
