7b90a7f005ea6b304411085865a458fc2b68f9cf
Skúli Arnarsson <vt_advania_sa@or.is>
2020-07-03 10:14:24 +0000

    add surveys sent in email

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -6,7 +6,10 @@ const surveyTemplate = require('../services/templates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    app.get('/api/surveys/thanks', (req, res) => {
+        res.send('Thanks for voting!');
+    })
+    app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
         const { title, subject, body, recipients } = req.body;
 
         const survey = new Survey({
@@ -20,6 +23,16 @@ module.exports = app => {
             dateSent: Date.now()
         });
         const mailer = new Mailer(survey, surveyTemplate(survey));
-        mailer.send();
+        try {
+            await mailer.send();
+            await survey.save();
+            req.user.credits -= 1;
+            const user = await req.user.save();
+
+            res.send(user);
+        }
+        catch (err) {
+            res.status(422).send(err);
+        }
     });
 };
--- services/Mailer.js
+++ services/Mailer.js
@@ -33,7 +33,7 @@ class Mailer extends helper.Mail {
 
     addRecipients() {
         const personalize = new helper.Personalization();
-        this.recipients.foreach(recipient => {
+        this.recipients.forEach(recipient => {
             personalize.addTo(recipient);
         });
         this.addPersonalization(personalize);
